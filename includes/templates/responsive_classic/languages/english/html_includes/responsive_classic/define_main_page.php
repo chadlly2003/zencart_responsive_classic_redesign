4ad3a04c5f95dd5c34c3503b1d3a3aebf9583033
chadlly2003 <chad@invertersupply.com>
2025-01-24 19:10:19 -0500

    Delete includes/templates/responsive_classic/languages/english/html_includes/responsive_classic/define_main_page.php

--- includes/templates/responsive_classic/languages/english/html_includes/responsive_classic/define_main_page.php
+++
@@ -1,5 +0,0 @@
-<p class="biggerText">This site is fully responsive, providing an optimized layout for all devices including desktops, tablets, and mobile phones. The layout automatically adjusts to offer an ideal viewing experience based on the device being used, ensuring a smooth and user-friendly interface across any screen size.</p>
-    
-<p>This text is located in the file at: <code> /languages/english/html_includes/YOUR_TEMPLATE/define_main_page.php</code></p>
-
-<p><strong>NOTE: Always backup the files in<code> /languages/english/html_includes/your_template</code></strong></p>

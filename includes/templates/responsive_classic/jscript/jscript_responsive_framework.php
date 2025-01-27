becd0e62cd71348680043888d92fc7f23a2e924d
chadlly2003 <chad@invertersupply.com>
2025-01-27 18:34:44 -0500

    Create jscript_responsive_framework.php

---
+++ includes/templates/responsive_classic/jscript/jscript_responsive_framework.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @copyright Copyright 2003-2024 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Jeff Rutt 2024 Sep 17 Modified in v2.1.0-beta1 $
+ */
+?>
+
+<script>
+
+(function($) {
+$(document).ready(function() {
+
+$('#contentMainWrapper').addClass('onerow-fluid');
+ $('#mainWrapper').css({
+     'max-width': '100%',
+     'margin': 'auto'
+ });
+ $('#headerWrapper').css({
+     'max-width': '100%',
+     'margin': 'auto'
+ });
+ $('#navSuppWrapper').css({
+     'max-width': '100%',
+     'margin': 'auto'
+ });
+
+$('a[href="#top"]').click(function(){
+$('html, body').animate({scrollTop:0}, 'slow');
+return false;
+});
+
+$(".categoryListBoxContents").click(function() {
+window.location = $(this).find("a").attr("href");
+return false;
+});
+
+$('.centeredContent').matchHeight();
+$('.specialsListBoxContents').matchHeight();
+$('.centerBoxContentsAlsoPurch').matchHeight();
+$('.categoryListBoxContents').matchHeight();
+
+$('.no-fouc').removeClass('no-fouc');
+});
+
+}) (jQuery);
+
+</script>

80bcc51c564ccab743a71f7a8d058f3317f00256
chadlly2003 <chad@invertersupply.com>
2025-02-12 23:35:05 -0500

    #26 Update tpl_reviews_default.php

--- includes/templates/responsive_classic/templates/tpl_reviews_default.php
+++ includes/templates/responsive_classic/templates/tpl_reviews_default.php
@@ -29,11 +29,10 @@
 ?>
 <div class="prod-list-wrap group">
 <div id="reviewsDefaultListingTopNumber" class="navSplitPagesResult back"><?php echo $reviews_split->display_count(TEXT_DISPLAY_NUMBER_OF_REVIEWS); ?></div>
- 
 </div>
 
 
-<div><div id="reviewsDefaultListingTopLinks" class="navSplitPagesLinks"><?php echo TEXT_RESULT_PAGE . $reviews_split->display_links($max_display_page_links, zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $paginateAsUL); ?></div></div>
+<div id="reviewsDefaultListingTopLinks" class="navSplitPagesLinks"><?php echo TEXT_RESULT_PAGE . $reviews_split->display_links($max_display_page_links, zen_get_all_get_params(array('page', 'info', 'x', 'y', 'main_page')), $paginateAsUL); ?></div>
 <?php
     }
 

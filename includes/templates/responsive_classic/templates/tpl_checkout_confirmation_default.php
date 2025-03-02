c06b35572513eae304ce27cc85800e2ec897f6b3
chadlly2003 <chad@invertersupply.com>
2025-03-01 21:17:01 -0500

    Update tpl_checkout_confirmation_default.php

--- includes/templates/responsive_classic/templates/tpl_checkout_confirmation_default.php
+++ includes/templates/responsive_classic/templates/tpl_checkout_confirmation_default.php
@@ -80,7 +80,7 @@
 
 <div class="group" id="order-comments">
 
-  <h2 id="checkoutConfirmDefaultHeadingComments"><?php echo HEADING_ORDER_COMMENTS; ?></h2>
+  <h5 id="checkoutConfirmDefaultHeadingComments"><?php echo HEADING_ORDER_COMMENTS; ?></h5>
   <div class="buttonRow editelement"><?php echo  '<a href="' . zen_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL') . '">' . zen_image_button(BUTTON_IMAGE_EDIT_SMALL, BUTTON_EDIT_SMALL_ALT) . '</a>'; ?></div>
   <div><?php echo (empty($order->info['comments']) ? NO_COMMENTS_TEXT : nl2br(zen_output_string_protected($order->info['comments'])) . zen_draw_hidden_field('comments', $order->info['comments'])); ?></div>
 

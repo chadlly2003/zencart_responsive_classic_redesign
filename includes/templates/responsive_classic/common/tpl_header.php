d95d32d3aa16c92c107a53019f9931bd712e1ce7
chadlly2003 <chad@invertersupply.com>
2025-03-02 00:17:21 -0500

    Update tpl_header.php

--- includes/templates/responsive_classic/common/tpl_header.php
+++ includes/templates/responsive_classic/common/tpl_header.php
@@ -35,7 +35,8 @@ if (isset($flag_disable_header) && $flag_disable_header === true) {
 }
 ?>
  
-
+ <a href="<?php echo $_SERVER['REQUEST_URI']; ?>#main-content" class="skip-link">Skip to main content</a>
+ 
 <div class="welcome_note">Welcome to Zencart check out the getting started guide at 
   <a href="https://docs.zen-cart.com/user/" target="_blank" class="welcome_link"  rel="noopener"> doc.zencart.com</a>
 </div>

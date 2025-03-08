0f4bb495d3218a951c06c44a20928c3490967e00
chadlly2003 <chad@invertersupply.com>
2025-03-07 22:18:14 -0500

    Update jscript_sidebox_select_form.php

--- includes/templates/responsive_classic/jscript/jscript_sidebox_select_form.php
+++ includes/templates/responsive_classic/jscript/jscript_sidebox_select_form.php
@@ -1,50 +1,53 @@
 <script>
 jQuery(document).ready(function() {
-    // -----
-    // Cycle through each form with a class of 'sidebox-select-form' that has a select tag
-    // marked as 'required'.
+    // Cycle through each form with a class of 'sidebox-select-form' that has a select tag marked as 'required'.
     jQuery('form.sidebox-select-form select:required').each(function() {
-        // -----
-        // Iterate over each of the select tag's options, 'converting' any option with a value
-        // of '' into an <optgroup>, adding the other options as <option> tags for that 'group' and then closing
-        // up the <optgroup> tag.
-        var theOptions = '';
-        var optGroup = false;
-        var isSelected = '';
-        jQuery('option', this).each(function() {
-            if (jQuery(this).val() === '') {
-                // Make sure "Please Select" is shown correctly on both desktop and mobile
-                theOptions += '<option value="" disabled selected visible>' + jQuery(this).text() + '</option>';
+        var $select = jQuery(this);
+        
+        // Create the "Please Select" option (disabled and selected)
+        var $pleaseSelectOption = jQuery('<option>', {
+            value: '',
+            disabled: true,
+            selected: true,
+            text: 'Please Select' // Placeholder text
+        });
+
+        // Prepend the "Please Select" option to the select element if it's not already there
+        if ($select.find('option[value=""]').length === 0) {
+            $select.prepend($pleaseSelectOption);
+        }
+
+        // Disable the "Please Select" option after a valid selection
+        $select.on('change', function() {
+            var selectedValue = $select.val();
+
+            // If a valid option is selected, make sure "Please Select" is disabled
+            if (selectedValue) {
+                $select.find('option[value=""]').prop('disabled', true); // Disable "Please Select"
             } else {
-                isSelected = '';
-                if (jQuery(this).is(':selected')) {
-                    isSelected = ' selected="selected"';
-                }
-                theOptions += '<option value="'+jQuery(this).val()+'"'+isSelected+'>'+jQuery(this).text()+'</option>';
+                $select.find('option[value=""]').prop('disabled', false); // Enable "Please Select" if nothing is selected
+            }
+
+            // Enable/Disable submit button based on selection
+            if (selectedValue) {
+                $select.siblings('input[type="submit"], button[type="submit"]').prop('disabled', false).css('cursor', 'pointer');
+            } else {
+                $select.siblings('input[type="submit"], button[type="submit"]').prop('disabled', true).css('cursor', 'not-allowed');
             }
         });
-        if (optGroup === true) {
-            theOptions += '</optgroup>';
-        }
-        jQuery(this).empty().append(theOptions);
-        jQuery('optgroup', this).css({'font-style':'normal'});
 
-        // -----
-        // If a non-'' option is currently selected, ensure that the form's submit button is
-        // enabled and change the cursor to a pointer.  Otherwise, disable the form's submit
-        // button and change the cursor to indicate that the button is not allowed.
-        if (jQuery('select option:selected', this).length > 0) {
-            jQuery(this).siblings('input[type="submit"], button[type="submit"]').attr('disabled', false).css('cursor', 'pointer');
-        } else {
-            jQuery(this).siblings('input[type="submit"], button[type="submit"]').attr('disabled', true).css('cursor', 'not-allowed');
-        }
-
-        // -----
-        // If an option in the select tag is selected, re-enable the submit button and change the
-        // cursor back to a pointer.
-        jQuery(this).on('change', function() {
-            jQuery(this).siblings('input[type="submit"], button[type="submit"]').attr('disabled', false).css('cursor', 'pointer');
+        // Reset functionality: If the form is reset, enable "Please Select" option again
+        $select.closest('form').on('reset', function() {
+            // Re-enable the "Please Select" option
+            $select.find('option[value=""]').prop('disabled', false);
+            // Disable submit button as no option is selected
+            $select.siblings('input[type="submit"], button[type="submit"]').prop('disabled', true).css('cursor', 'not-allowed');
         });
+
+        // Initial check: If the form is already populated (pre-selected value), disable "Please Select"
+        if ($select.val()) {
+            $select.find('option[value=""]').prop('disabled', true);
+        }
     });
 });
 </script>

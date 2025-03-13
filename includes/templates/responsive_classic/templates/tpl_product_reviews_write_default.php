f7d0554186a334bbd25d4ebda5a88dc10643efdb
chadlly2003 <chad@invertersupply.com>
2025-03-13 19:35:03 -0400

    Update tpl_product_reviews_write_default.php

--- includes/templates/responsive_classic/templates/tpl_product_reviews_write_default.php
+++ includes/templates/responsive_classic/templates/tpl_product_reviews_write_default.php
@@ -38,34 +38,38 @@
  
  
 <div id="reviews-right">
-<h2 id="reviewsWritePrice"><?php echo $products_price; ?></h2> 
-<h3 id="reviewsWriteReviewer"><?php echo SUB_TITLE_FROM . '&nbsp;&nbsp;', zen_output_string_protected($reviewer->fields['customers_firstname'] . ' ' . $reviewer->fields['customers_lastname']); ?></h3>
+  <h2 id="reviewsWritePrice"><?php echo $products_price; ?></h2> 
+  <h3 id="reviewsWriteReviewer"><?php echo SUB_TITLE_FROM . '&nbsp;&nbsp;', zen_output_string_protected($reviewer->fields['customers_firstname'] . ' ' . $reviewer->fields['customers_lastname']); ?></h3>
 
 <?php if ($messageStack->size('review_text') > 0) echo $messageStack->output('review_text'); ?>
-<div id="reviewsWriteReviewsRate" class="center"><?php echo SUB_TITLE_RATING; ?> 
+      <div id="reviewsWriteReviewsRate" class="center"><?php echo SUB_TITLE_RATING; ?> 
 
-<div class="ratingRow">
-<?php echo zen_draw_radio_field('rating', '1', '', 'id="rating-1"'); ?>
-<?php echo '<label class="" for="rating-1">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_ONE, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_ONE, OTHER_REVIEWS_RATING_STARS_ONE_ALT) . '</label> '; ?>
+        <div class="ratingRow">
+        <?php echo zen_draw_radio_field('rating', '1', '', 'id="rating-1"'); ?>
+        <?php echo '<label class="" for="rating-1">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_ONE, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_ONE, OTHER_REVIEWS_RATING_STARS_ONE_ALT) . '</label> '; ?>
 
-<?php echo zen_draw_radio_field('rating', '2', '', 'id="rating-2"'); ?>
-<?php echo '<label class="" for="rating-2">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_TWO, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_TWO, OTHER_REVIEWS_RATING_STARS_TWO_ALT) . '</label>'; ?>
+        <?php echo zen_draw_radio_field('rating', '2', '', 'id="rating-2"'); ?>
+        <?php echo '<label class="" for="rating-2">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_TWO, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_TWO, OTHER_REVIEWS_RATING_STARS_TWO_ALT) . '</label>'; ?>
 
-<?php echo zen_draw_radio_field('rating', '3', '', 'id="rating-3"'); ?>
-<?php echo '<label class="" for="rating-3">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_THREE, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_THREE, OTHER_REVIEWS_RATING_STARS_THREE_ALT) . '</label>'; ?>
+        <?php echo zen_draw_radio_field('rating', '3', '', 'id="rating-3"'); ?>
+        <?php echo '<label class="" for="rating-3">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_THREE, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_THREE, OTHER_REVIEWS_RATING_STARS_THREE_ALT) . '</label>'; ?>
 
-<?php echo zen_draw_radio_field('rating', '4', '', 'id="rating-4"'); ?>
-<?php echo '<label class="" for="rating-4">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_FOUR, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_FOUR, OTHER_REVIEWS_RATING_STARS_FOUR_ALT) . '</label>'; ?>
+        <?php echo zen_draw_radio_field('rating', '4', '', 'id="rating-4"'); ?>
+        <?php echo '<label class="" for="rating-4">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_FOUR, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_FOUR, OTHER_REVIEWS_RATING_STARS_FOUR_ALT) . '</label>'; ?>
 
-<?php echo zen_draw_radio_field('rating', '5', '', 'id="rating-5"'); ?>
-<?php echo '<label class="" for="rating-5">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_FIVE, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_FIVE, OTHER_REVIEWS_RATING_STARS_FIVE_ALT) . '</label>'; ?>
+        <?php echo zen_draw_radio_field('rating', '5', '', 'id="rating-5"'); ?>
+        <?php echo '<label class="" for="rating-5">' . zen_image($template->get_template_dir(OTHER_IMAGE_REVIEWS_RATING_STARS_FIVE, DIR_WS_TEMPLATE, $current_page_base,'images'). '/' . OTHER_IMAGE_REVIEWS_RATING_STARS_FIVE, OTHER_REVIEWS_RATING_STARS_FIVE_ALT) . '</label>'; ?>
+      </div>
+    </div>
+  </div>
 </div>
-</div>
-<label id="textAreaReviews" for="review-text"><?php echo SUB_TITLE_REVIEW; ?></label>
-<?php echo zen_draw_textarea_field('review_text', 60, 5, '', 'id="review-text"'); ?>
-<?php echo zen_draw_input_field($antiSpamFieldName, '', ' size="40" id="CUAS" style="visibility:hidden; display:none;" autocomplete="off"'); ?>
 
-</div></div>
+<div class="write_reviews_container">
+  <label id="textAreaReviews" for="review-text"><?php echo SUB_TITLE_REVIEW; ?></label>
+  <?php echo zen_draw_textarea_field('review_text', 60, 5, '', 'id="review-text"'); ?>
+  <?php echo zen_draw_input_field($antiSpamFieldName, '', ' size="40" id="CUAS" style="visibility:hidden; display:none;" autocomplete="off"'); ?>
+</div>
+
 <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_SUBMIT, BUTTON_SUBMIT_ALT); ?></div>
 <div id="reviewsWriteReviewsNotice" class="notice"><?php echo TEXT_NO_HTML . (REVIEWS_APPROVAL == '1' ? '<br>' . TEXT_APPROVAL_REQUIRED: ''); ?></div>
 

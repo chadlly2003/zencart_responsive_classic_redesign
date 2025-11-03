c86aa25348f6b6817558d59b2bdb83e4724c3cea
chadlly2003 <chad@invertersupply.com>
2025-11-02 21:32:18 -0500

    #69 Update tpl_categories.php

--- includes/templates/responsive_classic/sideboxes/tpl_categories.php
+++ includes/templates/responsive_classic/sideboxes/tpl_categories.php
@@ -59,16 +59,16 @@ for ($i=0, $j=sizeof($box_categories_array); $i<$j; $i++) {
     }
 }
 
-if (SHOW_CATEGORIES_BOX_SPECIALS === 'true' || SHOW_CATEGORIES_BOX_PRODUCTS_NEW === 'true' || SHOW_CATEGORIES_BOX_FEATURED_PRODUCTS === 'true' || SHOW_CATEGORIES_BOX_PRODUCTS_ALL === 'true' || SHOW_CATEGORIES_BOX_FEATURED_CATEGORIES === 'true') {
-    // display a separator between categories and links
-    if (SHOW_CATEGORIES_SEPARATOR_LINK === '1') {
-        $content .= '' . "\n";
+if (SHOW_CATEGORIES_BOX_SPECIALS == 'true' or SHOW_CATEGORIES_BOX_PRODUCTS_NEW == 'true' or SHOW_CATEGORIES_BOX_FEATURED_PRODUCTS == 'true' or SHOW_CATEGORIES_BOX_PRODUCTS_ALL == 'true') {
+// display a separator between categories and links
+    if (SHOW_CATEGORIES_SEPARATOR_LINK == '1') {
+      $content .= '<hr id="catBoxDivider">' . "\n";
     }
-    if (SHOW_CATEGORIES_BOX_SPECIALS === 'true') {
-        $show_this = $db->Execute("SELECT products_id FROM " . TABLE_SPECIALS . " WHERE status= 1 limit 1");
-        if ($show_this->EOF) {
-            $content .= '<li><a class="category-links" href="' . zen_href_link(FILENAME_SPECIALS) . '">' . CATEGORIES_BOX_HEADING_SPECIALS . '</a></li>' . "\n";
-        }
+    if (SHOW_CATEGORIES_BOX_SPECIALS == 'true') {
+      $show_this = $db->Execute("select s.products_id from " . TABLE_SPECIALS . " s where s.status= 1 limit 1");
+      if ($show_this->RecordCount() > 0) {
+        $content .= '<li><a class="category-links" href="' . zen_href_link(FILENAME_SPECIALS) . '">' . CATEGORIES_BOX_HEADING_SPECIALS . '</a></li>' . '' . "\n";
+      }
     }
     if (SHOW_CATEGORIES_BOX_PRODUCTS_NEW === 'true') {
         // display limits

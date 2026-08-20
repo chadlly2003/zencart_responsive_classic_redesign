d362a244e8801eeb8053d33257af3bbd8551ac53
chadlly2003 <chad@invertersupply.com>
2026-08-20 13:11:07 -0400

    #95 Update tpl_login_default.php

--- includes/templates/responsive_classic/templates/tpl_login_default.php
+++ includes/templates/responsive_classic/templates/tpl_login_default.php
@@ -16,43 +16,44 @@
 
     <?php if (USE_SPLIT_LOGIN_MODE == 'True' || $ec_button_enabled) { ?>
         <!--BOF PPEC split login- DO NOT REMOVE-->
-        <fieldset class="floatingBox back">
-            <legend><?php echo HEADING_NEW_CUSTOMER_SPLIT; ?></legend>
+        <div class="login_layout_setting"> 
+            <fieldset class="floatingBox back">
+                <legend><?php echo HEADING_NEW_CUSTOMER_SPLIT; ?></legend>
 
-            <?php // ** BEGIN PAYPAL EXPRESS CHECKOUT ** ?>
-            <?php if ($ec_button_enabled) { ?>
-                <div class="information"><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION_SPLIT; ?></div>
-                <div class="center"><?php require(DIR_FS_CATALOG . DIR_WS_MODULES . 'payment/paypal/tpl_ec_button.php'); ?></div>
-                <hr/>
-                <?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_DIVIDER; ?>
-            <?php } ?>
-            <?php // ** END PAYPAL EXPRESS CHECKOUT ** ?>
+                <?php // ** BEGIN PAYPAL EXPRESS CHECKOUT ** ?>
+                <?php if ($ec_button_enabled) { ?>
+                    <div class="information"><?php echo TEXT_NEW_CUSTOMER_INTRODUCTION_SPLIT; ?></div>
+                    <div class="center"><?php require(DIR_FS_CATALOG . DIR_WS_MODULES . 'payment/paypal/tpl_ec_button.php'); ?></div>
+                    <hr/>
+                    <?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_DIVIDER; ?>
+                <?php } ?>
+                <?php // ** END PAYPAL EXPRESS CHECKOUT ** ?>
 
-            <div class="information"><?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_SPLIT; ?></div>
-            <?php echo zen_draw_form('create', zen_href_link(FILENAME_CREATE_ACCOUNT, (isset($_GET['gv_no']) ? '&gv_no=' . preg_replace('/[^0-9.,%]/', '', $_GET['gv_no']) : ''), 'SSL')); ?>
+                <div class="information"><?php echo TEXT_NEW_CUSTOMER_POST_INTRODUCTION_SPLIT; ?></div>
+                <?php echo zen_draw_form('create', zen_href_link(FILENAME_CREATE_ACCOUNT, (isset($_GET['gv_no']) ? '&gv_no=' . preg_replace('/[^0-9.,%]/', '', $_GET['gv_no']) : ''), 'SSL')); ?>
 
-            <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CREATE_ACCOUNT, BUTTON_CREATE_ACCOUNT_ALT, 'name="registrationButton"'); ?></div>
-            <?php echo '</form>'; ?>
-        </fieldset>
+                <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_CREATE_ACCOUNT, BUTTON_CREATE_ACCOUNT_ALT, 'name="registrationButton"'); ?></div>
+                <?php echo '</form>'; ?>
+            </fieldset>
 
-        <fieldset class="floatingBox forward">
-            <legend><?php echo HEADING_RETURNING_CUSTOMER_SPLIT; ?></legend>
-            <div class="information2"><?php echo TEXT_RETURNING_CUSTOMER_SPLIT; ?></div>
-            <?php echo zen_draw_form('loginForm', zen_href_link(FILENAME_LOGIN, 'action=process' . (isset($_GET['gv_no']) ? '&gv_no=' . preg_replace('/[^0-9.,%]/', '', $_GET['gv_no']) : ''), 'SSL'), 'post', 'id="loginForm"'); ?>
+            <fieldset class="floatingBox forward">
+                <legend><?php echo HEADING_RETURNING_CUSTOMER_SPLIT; ?></legend>
+                <div class="information2"><?php echo TEXT_RETURNING_CUSTOMER_SPLIT; ?></div>
+                <?php echo zen_draw_form('loginForm', zen_href_link(FILENAME_LOGIN, 'action=process' . (isset($_GET['gv_no']) ? '&gv_no=' . preg_replace('/[^0-9.,%]/', '', $_GET['gv_no']) : ''), 'SSL'), 'post', 'id="loginForm"'); ?>
 
-            <label class="inputLabel" for="login-email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
-            <?php echo zen_draw_input_field('email_address', '', 'size="18" id="login-email-address" autofocus autocomplete="username" placeholder="' . ENTRY_EMAIL_ADDRESS_TEXT . '"' . ((int)ENTRY_EMAIL_ADDRESS_MIN_LENGTH > 0 ? ' required' : ''), 'email'); ?>
-            <br class="clearBoth">
+                <label class="inputLabel" for="login-email-address"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
+                <?php echo zen_draw_input_field('email_address', '', 'size="18" id="login-email-address" autofocus autocomplete="username" placeholder="' . ENTRY_EMAIL_ADDRESS_TEXT . '"' . ((int)ENTRY_EMAIL_ADDRESS_MIN_LENGTH > 0 ? ' required' : ''), 'email'); ?>
+                <br class="clearBoth">
 
-            <label class="inputLabel" for="login-password"><?php echo ENTRY_PASSWORD; ?></label>
-            <?php echo zen_draw_password_field('password', '', 'size="18" id="login-password" autocomplete="current-password" placeholder="' . ENTRY_REQUIRED_SYMBOL . '"' . ((int)ENTRY_PASSWORD_MIN_LENGTH > 0 ? ' required' : '')); ?>
-            <br class="clearBoth">
+                <label class="inputLabel" for="login-password"><?php echo ENTRY_PASSWORD; ?></label>
+                <?php echo zen_draw_password_field('password', '', 'size="18" id="login-password" autocomplete="current-password" placeholder="' . ENTRY_REQUIRED_SYMBOL . '"' . ((int)ENTRY_PASSWORD_MIN_LENGTH > 0 ? ' required' : '')); ?>
+                <br class="clearBoth">
 
-            <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_LOGIN, BUTTON_LOGIN_ALT); ?></div>
-            <div class="buttonRow back important"><?php echo '<a href="' . zen_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></div>
-            <?php echo '</form>'; ?>
-        </fieldset>
-
+                <div class="buttonRow forward"><?php echo zen_image_submit(BUTTON_IMAGE_LOGIN, BUTTON_LOGIN_ALT); ?></div>
+                <div class="buttonRow back important"><?php echo '<a href="' . zen_href_link(FILENAME_PASSWORD_FORGOTTEN, '', 'SSL') . '">' . TEXT_PASSWORD_FORGOTTEN . '</a>'; ?></div>
+                <?php echo '</form>'; ?>
+            </fieldset>
+    </div>
         <br class="clearBoth">
         <!--EOF PPEC split login- DO NOT REMOVE-->
     <?php } else { ?>

2e34bf8da2f4770ec3a4610541be542f6130003e
chadlly2003 <chad@invertersupply.com>
2026-08-20 12:59:24 -0400

    #94 Create jscript_image_modals.js

---
+++ includes/templates/responsive_classic/jscript/jscript_image_modals.js
@@ -0,0 +1,11 @@
+// JavaScript to show and hide the image modals
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
